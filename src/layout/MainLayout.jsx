0afe9dc1cce174f469c4608bc95c734198e8519d
searchsakib <searchsakib@gmail.com>
2023-09-24 23:35:37 +0600

    first commit

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,12 @@
+import Home from '../pages/Home/Home';
+
+const MainLayout = () => {
+  return (
+    <div>
+      <h2>Hello world boys!</h2>
+      <Home></Home>
+    </div>
+  );
+};
+
+export default MainLayout;
