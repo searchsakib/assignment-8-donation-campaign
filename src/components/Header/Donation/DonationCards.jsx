0a880534f60da40195f7f44e4b3858f4affe7180
searchsakib <searchsakib@gmail.com>
2023-09-25 18:32:35 +0600

    adding route for donation details & some minor bug fix

--- src/components/Header/Donation/DonationCards.jsx
+++ src/components/Header/Donation/DonationCards.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const DonationCards = ({ donation }) => {
   const { id, image, category, category_bg, title, text_color, card_bg } =
     donation || {};
@@ -5,32 +7,34 @@ const DonationCards = ({ donation }) => {
   console.log(card_bg);
   return (
     <div className="gap-6 ">
-      <div
-        className={`card card-compact shadow-sm`}
-        style={{ backgroundColor: `${card_bg}` }}
-      >
-        <figure>
-          <img className="w-full" src={image} alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h4
-            className="card-title text-base font-medium w-fit py-1 px-[10px] rounded "
-            style={{
-              backgroundColor: `${category_bg}`,
-              color: `${text_color}`,
-            }}
-          >
-            {category}
-          </h4>
-          <h2
-            style={{ color: `${text_color}` }}
-            className="font-semibold text-xl"
-          >
-            {' '}
-            {title}{' '}
-          </h2>
+      <Link to={`/donation/${id}`}>
+        <div
+          className={`card card-compact shadow-sm`}
+          style={{ backgroundColor: `${card_bg}` }}
+        >
+          <figure>
+            <img className="w-full" src={image} alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h4
+              className="card-title text-base font-medium w-fit py-1 px-[10px] rounded "
+              style={{
+                backgroundColor: `${category_bg}`,
+                color: `${text_color}`,
+              }}
+            >
+              {category}
+            </h4>
+            <h2
+              style={{ color: `${text_color}` }}
+              className="font-semibold text-xl"
+            >
+              {' '}
+              {title}{' '}
+            </h2>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
