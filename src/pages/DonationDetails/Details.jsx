3bac70eb38a1d9b7ddf5ef7ca919e0645c32e6a5
searchsakib <searchsakib@gmail.com>
2023-09-25 20:22:52 +0600

    margin bottom added in donation details

--- src/pages/DonationDetails/Details.jsx
+++ src/pages/DonationDetails/Details.jsx
@@ -12,22 +12,13 @@ const Details = ({ donationDetails }) => {
             layout="fill"
           />
         </div>
-        <div>
+        <div className="mb-28">
           <h5 className="mb-2 block font-sans text-[40px] text-[#0B0B0B] pt-14 pb-4 font-bold leading-snug tracking-normal  antialiased">
             {title}
           </h5>
           <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
             {description}
           </p>
-        </div>
-        <div className="p-6 pt-0">
-          <button
-            className="select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            type="button"
-            data-ripple-light="true"
-          >
-            Read More
-          </button>
         </div>
       </div>
     </div>
