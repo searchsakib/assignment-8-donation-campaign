fee3c974798d9b19cc8fd628d00d008c667a207b
searchsakib <searchsakib@gmail.com>
2023-09-26 15:04:52 +0600

    pie chart half working

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import React, { PureComponent } from 'react';
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Cell, ResponsiveContainer, Label } from 'recharts';
+
 const Statistics = () => {
   const [donation, setDonation] = useState([]);
   const [notFound, setNotFound] = useState(false);
@@ -23,35 +24,40 @@ const Statistics = () => {
   ];
 
   const COLORS = ['#0088FE', '#00C49F'];
-  console.log(donation.length);
 
   return (
     <div className="max-w-[1320px] mx-auto">
       {notFound ? (
         <p className="text-4xl font-bold text-green-500 text-center mt-24 mb-10 px-6 md:px-10 lg:px-10 xl:px-0">
-          {' '}
-          {notFound}{' '}
+          {notFound}
         </p>
       ) : (
         <div>
-          <PieChart width={400} height={400}>
-            <Pie
-              data={data}
-              cx="50%"
-              cy="50%"
-              labelLine={false}
-              outerRadius={80}
-              fill="#8884d8"
-              dataKey="value"
-            >
-              {data.map((entry, index) => (
-                <Cell
-                  key={`cell-${index}`}
-                  fill={COLORS[index % COLORS.length]}
+          <ResponsiveContainer width="100%" height={400}>
+            <PieChart>
+              <Pie
+                data={data}
+                cx="50%"
+                cy="50%"
+                labelLine={false}
+                outerRadius={80}
+                fill="#8884d8"
+                dataKey="value"
+              >
+                {data.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                  />
+                ))}
+                <Label
+                  value={`${((data[0].value / allData) * 100).toFixed(2)}%`}
+                  position="insideBottom"
+                  fill="#fff"
                 />
-              ))}
-            </Pie>
-          </PieChart>
+              </Pie>
+            </PieChart>
+          </ResponsiveContainer>
         </div>
       )}
     </div>
