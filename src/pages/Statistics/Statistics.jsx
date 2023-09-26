1fcbf63c4120330c3e153fbef7449aa8b79a7b92
searchsakib <searchsakib@gmail.com>
2023-09-26 17:31:07 +0600

    pie chart working :)

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -1,8 +1,40 @@
-import { useEffect, useState } from 'react';
-import React, { PureComponent } from 'react';
-import { PieChart, Pie, Cell, ResponsiveContainer, Label } from 'recharts';
+import { PieChart, Pie, Cell } from 'recharts';
+import { useState, useEffect } from 'react';
 
-const Statistics = () => {
+// const data = [
+//   { name: 'Group A', value: 12 },
+//   { name: 'Group B', value: 300 },
+// ];
+
+const COLORS = ['#FF444A', '#00C49F'];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index,
+}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? 'start' : 'end'}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+export default function Statistics() {
   const [donation, setDonation] = useState([]);
   const [notFound, setNotFound] = useState(false);
 
@@ -23,74 +55,50 @@ const Statistics = () => {
     { name: 'Remaining', value: remainingValue },
   ];
 
-  const colors = ['#00C49F', '#FF444A'];
-
-  return (
-    <div className="max-w-[1320px] mx-auto">
-      {notFound ? (
-        <p className="text-4xl font-bold text-green-500 text-center mt-24 mb-10 px-6 md:px-10 lg:px-10 xl:px-0">
-          {notFound}
-        </p>
-      ) : (
-        <div>
-          <PieChart width={400} height={400}>
-            <Pie
-              dataKey="value"
-              isAnimationActive={false}
-              data={data}
-              cx="50%"
-              cy="50%"
-              outerRadius={80}
-              fill="#8884d8"
-            />
-            <Label
-              value={`${((data[0].value / allData) * 100).toFixed(2)}%`}
-              position="insideTopLeft"
-              fill="#fff"
-            />
-            <Label
-              value={`${((data[1].value / allData) * 100).toFixed(2)}%`}
-              position="outside"
-              fill="#f2a"
-            />
-          </PieChart>
-        </div>
-      )}
+  return notFound ? (
+    <p className="text-4xl font-bold text-green-500 text-center mt-24 mb-10 px-6 md:px-10 lg:px-10 xl:px-0">
+      {' '}
+      {notFound}{' '}
+    </p>
+  ) : (
+    <div className="max-w-[1320px] mx-auto ">
+      <div className="flex items-center justify-center ">
+        <PieChart width={400} height={400}>
+          <Pie
+            data={data}
+            cx={200}
+            cy={200}
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+        </PieChart>
+      </div>
     </div>
   );
-};
-
-export default Statistics;
-
-{
-  /* <ResponsiveContainer height={500}>
-            <PieChart>
-              <Pie
-                data={data}
-                cx="50%"
-                cy="50%"
-                labelLine={false}
-                outerRadius={80}
-                fill="#8884d8"
-                dataKey="value"
-              >
-                {data.map((entry, index) => (
-                  <Cell
-                    key={`cell-${index}`}
-                    fill={COLORS[index % COLORS.length]}
-                  />
-                ))}
-                <Label
-                  value={`${((data[0].value / allData) * 100).toFixed(2)}%`}
-                  position="insideTopLeft"
-                  fill="#fff"
-                />
-                <Label
-                  value={`${((data[1].value / allData) * 100).toFixed(2)}%`}
-                  position="outside"
-                  fill="#f2a"
-                />
-              </Pie>
-            </PieChart>
-          </ResponsiveContainer> */
 }
+
+// const Statistics = () => {
+// const [donation, setDonation] = useState([]);
+// const [notFound, setNotFound] = useState(false);
+
+// useEffect(() => {
+//   const myDonation = JSON.parse(localStorage.getItem('donation'));
+
+//   if (myDonation) {
+//     setDonation(myDonation);
+//   } else {
+//     setNotFound('No Donations Received Yet...');
+//   }
+// }, []);
+
+// const allData = 12;
