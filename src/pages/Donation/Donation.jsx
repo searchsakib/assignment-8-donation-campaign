834b625cbf84dfccd96f5ff81f93c51948d89d7a
searchsakib <searchsakib@gmail.com>
2023-09-26 08:27:22 +0600

    my donation card added

--- src/pages/Donation/Donation.jsx
+++ src/pages/Donation/Donation.jsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from 'react';
+import MyDonationCard from './MyDonationCard';
+
 const Donation = () => {
+  const [donation, setDonation] = useState([]);
+  const [notFound, setNotFound] = useState(false);
+
+  useEffect(() => {
+    const myDonation = JSON.parse(localStorage.getItem('donation'));
+
+    if (myDonation) {
+      setDonation(myDonation);
+    } else {
+      setNotFound('No data Found');
+    }
+  }, []);
+
+  console.log(donation);
+  console.log(notFound);
+
   return (
     <div>
-      <h2>This is Donation</h2>
+      {notFound ? (
+        <p className="text-4xl font-bold text-red-500 text-center">
+          {' '}
+          {notFound}{' '}
+        </p>
+      ) : (
+        <div>
+          {' '}
+          {donation?.map((donate) => (
+            <MyDonationCard key={donate.id} donate={donate}></MyDonationCard>
+          ))}{' '}
+        </div>
+      )}
     </div>
   );
 };
